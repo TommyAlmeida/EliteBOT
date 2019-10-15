6c2978abf5f6d537e740915474294f97732e8cbd
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-15 15:49:16 -0300

    Fixing ready.js event

--- eventos/ready.js
+++ eventos/ready.js
@@ -6,12 +6,12 @@ exports.name = 'ready';
 exports.run = (client) => {
     gameLoop.run(client);
 
-    const guild = client.guilds.get(c.guildID)
+    const guildID = client.guilds.get(c.guildID)
     const avatars = ["https://i.imgur.com/QDKX7zR.png", "https://i.imgur.com/JaCQELZ.png"]
 
     setInterval(async() => {
         const r = avatars[Math.floor(Math.random() * avatars.length)];
-        await guild.setIcon(r);
+        await guildID.setIcon(r);
     }, INTERVAL)
 
     console.log(`O Bot foi iniciado completamente com ${client.users.size} usuarios em ${client.guilds.size} servidores`);
